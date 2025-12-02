1dfe52d6c745ff413e5002a64797df84d05627bb
Temurkan <famill1949@gmail.com>
2025-12-02 16:54:07 +0500

    vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-import { BrowserRouter } from "react-router-dom";
-import AppRoutes from "@/router";
+import { BrowserRouter } from 'react-router-dom'
+import AppRoutes from '@/router'
+import { Toaster } from 'sonner'
 
 function App() {
   return (
     <BrowserRouter>
+      <Toaster />
       <AppRoutes />
     </BrowserRouter>
-  );
+  )
 }
 
-export default App;
+export default App
